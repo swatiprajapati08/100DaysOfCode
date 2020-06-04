47a504c4c916656ef6bea2f0a82a545538aa3cf5
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-06-04 23:14:02 +0530

    Add files via upload

---
+++ STRING/patternSearch.java
@@ -0,0 +1,8 @@
+//Pattern search
+
+//https://practice.geeksforgeeks.org/problems/naive-pattern-search-1587115620/1/?track=SPC-Strings&batchId=155
+
+static boolean search(String pat, String txt)
+    {
+            return txt.indexOf(pat)!=-1;
+    }
