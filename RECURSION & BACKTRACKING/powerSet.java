1106f14aae3724eb6ca1458361161bd476632e39
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-06-05 21:49:01 +0530

    Add files via upload

---
+++ RECURSION & BACKTRACKING/powerSet.java
@@ -0,0 +1,26 @@
+//Power Set Using Recursion
+//https://practice.geeksforgeeks.org/problems/power-set-using-recursion/1/?track=SPCF-Recursion&batchId=155
+
+class LexSort
+{
+    // complete the function
+    static ArrayList<String> powerSet(String s)
+    {
+        ArrayList<String> res=new ArrayList<String>();
+        permute(s,0,"",res);
+        Collections.sort(res);
+        return res;
+    }
+    
+    static void permute(String s,int index,String out,ArrayList<String> ans)
+    {
+        if(index==s.length())
+        {
+            ans.add(out);
+            return;
+        }
+        permute(s,index+1,out+s.charAt(index),ans);
+        permute(s,index+1,out,ans);
+    }
+    
+}
