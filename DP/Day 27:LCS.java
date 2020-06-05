486d7527408101db782ba6d9df9a67fbbc1e40bb
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-06-05 10:51:29 +0530

    LCS

---
+++ DP/Day 27:LCS.java
@@ -0,0 +1,29 @@
+//Longest Common Subsequence
+
+//https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3311/
+
+/*Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.*/
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int dp[][]=new int [text1.length()+1][text2.length()+1];
+        
+        for(int i=0;i<=text1.length();i++)
+        {
+            for(int j=0;j<=text2.length();j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+                else if(text1.charAt(i-1)==text2.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    
+            }
+        }
+        return dp[text1.length()][text2.length()];
+        
+    }
+}
